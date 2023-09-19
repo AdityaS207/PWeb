3fdf9db5a0e51472ca3d218727ebbe67b1c56183
ADT <adityasitumorang12345@gmail.com>
2023-09-19 09:36:55 +0700

    try to make another calculate function but fail

--- Day3/Calculator.js
+++ Day3/Calculator.js
@@ -19,6 +19,33 @@ function equals(){
     }
 }
 
+// function calculate(){
+//     var input = document.getElementById("result");
+//     var numbers = input.split(/\+|-|\*|\//);
+//     var operators = input.split(/\d+/).filter(Boolean);
+//     var result = parseFloat(numbers[0]);
+
+//     for (var i = 1; i < numbers.length; i++) {
+//         var number = parseFloat(numbers[i]);
+//         var operator = operators[i - 1];
+
+//         if (operator === "+") {
+//             result += number;
+//         } else if (operator === "-") {
+//             result -= number;
+//         } else if (operator === "*") {
+//             result *= number;
+//         } else if (operator === "/") {
+//             if (number !== 0) {
+//                 result /= number;
+//             } else {
+//                 alert("Division by zero is not allowed.");
+//                 return NaN;
+//             }
+//         }
+//     }
+// }
+
 function del(){
     result = document.getElementById("result");
     result = result.slice(0,-1);
