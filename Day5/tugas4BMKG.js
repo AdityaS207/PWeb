bb599a664c65e95231506c26652a33ad4f1f732f
ADT <adityasitumorang12345@gmail.com>
2023-10-10 09:35:36 +0700

    just edit tugasBMKG but not using that

--- Day5/tugas4BMKG.js
+++ Day5/tugas4BMKG.js
@@ -1,12 +1,18 @@
 function fetchWeatherData() {
     const apiUrl = "https://data.bmkg.go.id/DataMKG/MEWS/DigitalForecast/DigitalForecast-JawaTimur.xml";
     const xhr = new XMLHttpRequest();
+    var parser,xmlDoc;
 
     xhr.open("GET", apiUrl, true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
-                const xmlData = xhr.responseXML;
+                const xmlData = xhr.responseText;
+                parser = new DOMParser();
+                xmlDoc = parser.parseFromString(xmlData, "text/xml");
+                const temperatur = xmlDoc.getElementsByTagName("parameter")[5].childNodes[1].childNodes[1].innerHTML;
+                console.log(xmlData);
+                console.log(temperatur);
                 const surabayaData = xmlData.querySelector('area[id="501306"]');
                 const weatherDataContainer = document.getElementById("weatherData");
                 let weatherHTML = "";
